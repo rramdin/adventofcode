edae327a3cab2942454a89ff85261a658561230d
Robby Ramdin <rramdin@limebrokerage.com>
2015-12-05 18:02:39 -0500

    Add tests

--- src/4.py
+++ src/4.py
@@ -1,17 +1,29 @@
 import md5
 import sys
+import testing
 
-key = sys.argv[1]
-print "Finding coin for: " + key
-
-def satisfies(key, i):
+def satisfies(key, i, match):
     v = key + str(i)
     m = md5.new(v)
     d = m.hexdigest()
-    return d.startswith('00000')
+    return d.startswith(match)
 
-i = 1
-while not satisfies(key, i):
-    i += 1
+def findMatch(key, match):
+    i = 1
+    while not satisfies(key, i, match):
+        i += 1
+    return i
 
-print i
+def test():
+    tests = {
+            'abcdef': 609043,
+            'pqrstuv': 1048970,
+            }
+    testing.runTests(lambda x: findMatch(x, '00000'), tests)
+
+key = sys.argv[1]
+if key == 'test':
+    test()
+else:
+    match = sys.argv[2]
+    print findMatch(key, match)
