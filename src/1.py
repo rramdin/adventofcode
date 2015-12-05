86d512593f2c6ba4216c7ccc2fe68c513ec1aba7
Robby Ramdin <rramdin@limebrokerage.com>
2015-12-05 17:03:56 -0500

    Add tests

--- src/1.py
+++ src/1.py
@@ -1,7 +1,63 @@
 import sys
+import testing
 
-f = open(sys.argv[1], 'r')
+def findBasement(s):
+    floor = 0
+    for i in range(len(s)):
+        c = s[i]
+        updown = 1 if c == '(' else -1 if c == ')' else 0
+        floor += updown
+        if floor < 0:
+            return i+1
+    return 0
 
-s = f.read()
+def count(s):
+    return s.count('(') - s.count(')')
 
-print (s.count('(') - s.count(')'))
+def run(name, fn):
+    f = open(name, 'r')
+    s = f.read()
+    print fn(s)
+
+def test():
+    def runTests(fn, tests):
+        for k,v in tests.iteritems():
+            result = fn(k)
+            if result == v:
+                print "PASS: '%s'" % (k)
+            else:
+                print "FALL: '%s' (got: %d; expected: %d)" % (k,result,v)
+                sys.exit(1)
+
+    # Part 1
+    tests = {
+            '(())': 0,
+            '()()': 0,
+            '(((': 3,
+            '(()(()(': 3,
+            '))(((((': 3,
+            '())': -1,
+            '))(': -1,
+            ')))': -3,
+            ')())())': -3,
+            }
+    runTests(count, tests)
+
+    # Part 2
+    tests = {
+            ')': 1,
+            '()())': 5,
+            }
+    runTests(findBasement, tests)
+
+
+fname = sys.argv[1]
+if fname == 'test':
+    test()
+else:
+    part = sys.argv[2]
+    if part == '1':
+        run(fname, count)
+    elif part == '2':
+        run(fname, findBasement)
+
