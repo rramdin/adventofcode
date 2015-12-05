1778e9f4c7c38ba8cf31adb358f4ecc57d6d6897
Robby Ramdin <rramdin@limebrokerage.com>
2015-12-05 16:30:16 -0500

    Finish 5

--- 5.py
+++ 5.py
@@ -1,8 +1,31 @@
-f = open('5.txt', 'r')
+import sys
+
+bad_strings = ('ab', 'cd', 'pq', 'xy')
+bad_map = dict((x,True) for x in bad_strings)
+
+f = open(sys.argv[1], 'r')
 
 result = 0
 
 for s in f:
     s = s.strip()
+    
+    # check vowel count
+    vowel_count = 0
+    for c in 'aeiou':
+        vowel_count += s.count(c)
+    if vowel_count < 3:
+        continue
+
+    # check pairs
+    dupe_cond = False
+    for i in range(len(s) - 1):
+        if s[i] == s[i+1]:
+            dupe_cond = True
+        if s[i]+s[i+1] in bad_map:
+            continue
+
+    if dupe_cond:
+        result += 1
 
 print result
