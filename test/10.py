993242df2378be19fec4e783236f08b890d83e15
Robby Ramdin <rramdin@limebrokerage.com>
2015-12-13 16:15:14 -0500

    Fix 10 test

--- test/10.py
+++ test/10.py
@@ -4,11 +4,11 @@ import testing
 def test():
    # Part 1
    tests = {
-         '1': '11',
-         '11': '21',
-         '21': '1211',
-         '1211': '111221',
-         '111221': '312211',
+         '1': len('11'),
+         '11': len('21'),
+         '21': len('1211'),
+         '1211': len('111221'),
+         '111221': len('312211'),
          }
    testing.runTests(lambda x: advent10.process(x, 1), tests)
 
